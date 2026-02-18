b73de7636570b94526d703267120ff32d06ffabd
MDairektuzs <markus.m.dietz@bmw.de>
2026-02-18 18:47:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/CV-Dietz-Markus/',
+}) => ({
   server: {
     host: "::",
     port: 8080,
