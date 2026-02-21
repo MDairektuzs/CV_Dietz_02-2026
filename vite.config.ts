1b323f8bfbcc0e4a0a3344ea772e466522ca0015
MDairektuzs <markus.m.dietz@bmw.de>
2026-02-21 21:00:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/CV-Dietz-Markus/',
-}) => ({
+export default defineConfig(({ mode }) => ({
+  base: "/CV_Dietz_02-2026/",
   server: {
     host: "::",
     port: 8080,
