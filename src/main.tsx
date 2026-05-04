546621345b2ed3ec225e6c76db6f85f9e5763289
MDairektuzs <markus.m.dietz@bmw.de>
2026-05-04 22:08:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import { createRoot } from "react-dom/client"; 
-import App from "./App.tsx"; 
-import "./index.css"; 
-
-const REPO_BASE = "/CV_Dietz_02-2026"; 
-
-// Wenn wir auf GitHub Pages unter /<repo>/ starten, normalisieren wir auf / 
-if (location.pathname.startsWith(REPO_BASE)) { 
-  const newPath = location.pathname.slice(REPO_BASE.length) || "/"; 
-  const newUrl = newPath + location.search + location.hash; 
-  history.replaceState(null, "", newUrl); 
-} 
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
